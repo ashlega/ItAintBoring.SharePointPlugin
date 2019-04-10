d35ab8e1ba58f8b6c145f5ddaca5c712cbd51591
ashlega <ashlega@yahoo.com>
2019-04-10 07:44:45 -0400

    Add project files.

---
+++ ItAintBoring.SharePointPLugin/SharePointClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ItAintBoring.SharePointPLugin
+{
+    public class SharePointClient
+    {
+        private string token = null;
+
+        string clientId = null;
+        string tenantId = null;
+        string clientKey = null;
+        string siteRoot = null;
+
+        public SharePointClient(string clientId, string clientKey, string tenantId, string siteRoot)
+        {
+            this.clientId = clientId;
+            this.tenantId = tenantId;
+            this.clientKey = clientKey;
+            this.siteRoot = siteRoot;
+            
+        }
+
+        public async Task<string> GetToken()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                
+                
+                try
+                {
+                    //StringContent queryString = new StringContent("");
+                    
+                    FormUrlEncodedContent content = new FormUrlEncodedContent(
+                        new[]
+                        {
+                            new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                            new KeyValuePair<string, string>("client_id", $"{clientId}@{tenantId}"),
+                            new KeyValuePair<string, string>("client_secret", clientKey),
+                            new KeyValuePair<string, string>("resource", $"00000003-0000-0ff1-ce00-000000000000/{siteRoot}@{tenantId}")
+                        });
+
+                    string url = $"https://accounts.accesscontrol.windows.net/{tenantId}/tokens/OAuth/2";
+                    HttpResponseMessage response = await httpClient.PostAsync(new Uri(url), content);
+
+                    //response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+
+                    response.EnsureSuccessStatusCode();
+                    Dictionary<string, string> tokenAttributes = new Dictionary<string, string>();
+                    string[] tokenData = responseBody.Split(',');
+                    foreach(var s in tokenData)
+                    {
+                        string[] pair = s.Split(':');
+                        tokenAttributes.Add(pair[0], pair[1]);
+                    }
+
+                    //StringContent queryString = new StringContent(data);
+                    //var response = httpClient.PostStringAsync().Result;
+                    //"access_token"
+                    response = response;
+                }
+                catch(Exception ex)
+                {
+                    ex = ex;
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ ItAintBoring.SharePointPLugin/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItAintBoring.SharePointPLugin
+{
+    class Program
+    {
+        static void Main()
+        {
+            //treecatsoftware.sharepoint.com
+            SharePointClient cli = new SharePointClient("c3d33316-25b9-4779-8ce0-8c9445dff29b", "JrcG47KYf9XJ6eayxWMYgGuFiwuW/390FK4wXg/7Fow=", "ee810a5c-034a-47b1-926d-c745ecaf201b", "treecatsoftware.sharepoint.com");
+
+            Task<string> task = Task.Run<string>(async () => await cli.GetToken());
+
+            var result = task.Result;
+        }
+    }
+}
